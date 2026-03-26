1a12b3a0ffb6abd9587083a1303f6784127b8034
robyy21 <robithullah98@gmail.com>
2026-03-26 15:21:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-print("APP JALAN")
+print("INI PASTI JALAN")
+import time
 
-import os
-from telegram.ext import ApplicationBuilder, CommandHandler
-
-async def start(update, context):
-    await update.message.reply_text("BOT HIDUP 🔥")
-
-def main():
-    print("BOT STARTING...")
-
-    app = ApplicationBuilder().token("8140221752:AAE0z6baYIBv_sjxGprAY4OXHEpwOLMfvmM").build()
-
-    app.add_handler(CommandHandler("start", start))
-
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+while True:
+    print("RUNNING...")
+    time.sleep(5)
